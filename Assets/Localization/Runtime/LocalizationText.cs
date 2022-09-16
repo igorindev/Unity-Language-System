1607edea24e4f87082a9eb4b93cc41ae6d167392
Igor Lamounier <56743478+igorindev@users.noreply.github.com>
2022-09-16 01:15:34 -0300

    Localization Assets

---
+++ Assets/Localization/Runtime/LocalizationText.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEditor;
+using UnityEngine;
+
+namespace Localization
+{
+    [DisallowMultipleComponent]
+    [CanEditMultipleObjects]
+    public class LocalizationText : Localization
+    {
+        [SerializeField] int index = 1;
+        [SerializeField] string contextIndex = "context_name_value";
+
+        TMP_Text text;
+
+        protected override void Initialize()
+        {
+            text = GetComponent<TMP_Text>();
+        }
+
+        public override void UpdateLocalization()
+        {
+            text.text = LocalizationManager.instance.GetContentAtIndex(index, gameObject);
+        }
+    }
+}
